9ce074b06cac29a42db07b7abe1b6d15aa2a2885
Lu Zhang <luzhang@jmlvyou.com>
2013-11-18 10:09:12 +0800

    Reorganize the files

---
+++ trunk/Suley.Blog/src/org/suley/blog/Main.java
@@ -0,0 +1,14 @@
+package org.suley.blog;
+import org.suley.blog.util.SystemUiHider;
+
+import android.annotation.TargetApi;
+import android.app.Activity;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.MotionEvent;
+import android.view.View;
+
+public class Main extends Activity{
+	
+}
